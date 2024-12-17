8c407611bb7b01a120464ae3bee29fd63b4ec022
AkashDeepSharmaX <akashdeepsharma665@gmail.com>
2024-12-17 23:44:09 +0530

    validation postsdtos

--- src/main/java/com/example/softrelic/dtos/PostDto.java
+++ src/main/java/com/example/softrelic/dtos/PostDto.java
@@ -1,11 +1,12 @@
 package com.example.softrelic.dtos;
 
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 public class PostDto {
     @NotNull(message = "Username can not be null")
     private String text;
-    @NotNull(message = "Username can not be null")
+    @NotEmpty(message = "Username can not be null")
     private String title;
 
     public String getText() {
